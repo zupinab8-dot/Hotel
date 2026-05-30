d714902397338c89c70a7ff0eb80cbe6a28a240d
Seu Nome <seuemail@exemplo.com>
2026-05-29 23:57:08 -0300

    Primeira versão do sistema de madeireira

---
+++ database/migrations/2026_05_30_023355_create_madeiras_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('madeiras', function (Blueprint $table) {
+        $table->id();
+        $table->string('tipo');
+        $table->decimal('preco', 10, 2);
+        $table->integer('quantidade');
+        $table->timestamps();
+    });
+}
+
+    public function down(): void
+    {
+        Schema::dropIfExists('madeiras');
+    }
+};
